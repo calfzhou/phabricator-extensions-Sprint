a1e806b3eb7dcd01f0e49e74caf1c6c60685a621
Ji Zhou <calf.zhou@gmail.com>
2015-05-05 16:11:13 +0800

    change root dir and some other constants

--- scripts/__init_script__.php
+++ scripts/__init_script__.php
@@ -2,10 +2,12 @@
 
 function init_phabricator_script() {
 
-  phutil_load_library('/srv/phab/libphutil/src');
-  phutil_load_library('/srv/phab/phabricator/src');
-  phutil_load_library('/srv/phab/Sprint/src');
-  phutil_load_library('/srv/phab/arcanist/src');
+  const ROOT_DIR = '/opt/phacility';
+
+  phutil_load_library("{$ROOT_DIR}/libphutil/src");
+  phutil_load_library("{$ROOT_DIR}/phabricator/src");
+  phutil_load_library("{$ROOT_DIR}/extensions/Sprint/src");
+  phutil_load_library("{$ROOT_DIR}/arcanist/src");
 
 
   PhabricatorEnv::initializeScriptEnvironment();
--- src/constants/SprintConstants.php
+++ src/constants/SprintConstants.php
@@ -5,11 +5,11 @@ final class SprintConstants {
   const CUSTOMFIELD_TYPE_STATUS  = 'core:customfield';
   const PHABRICATOR_ROOT_DIR = 'vendor/phabricator/phabricator';
   const LIBPHUTIL_ROOT_DIR = 'vendor/libphutil/libphutil';
-  const ROOT_DIR = '/srv/phab';
+  const ROOT_DIR = '/opt/phacility';
   const POINTFIELD_INDEX = 'yERhvoZPNPtM';
   const SPRINTFIELD_INDEX = 'scsOmkpB9Tqi';
   const TYPE_CLOSED_STATUS_COLUMN = 'Done';
-  const TYPE_REVIEW_STATUS_COLUMN = 'Review';
-  const TYPE_DOING_STATUS_COLUMN = 'Doing';
+  const TYPE_REVIEW_STATUS_COLUMN = 'Verifying';
+  const TYPE_DOING_STATUS_COLUMN = 'Working';
   const TYPE_BACKLOG_STATUS_COLUMN = 'Backlog';
 }
